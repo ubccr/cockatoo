9404dce6f5beb18abec57e3b72ad103a2921ed06
Andrew E. Bruno <aebruno2@buffalo.edu>
2013-12-02 00:03:13 -0500

    Add check for missing fp

--- cockatoo/metric.py
+++ cockatoo/metric.py
@@ -44,6 +44,8 @@ def _centroid(cocktail):
 
     centroid = {}
     for cp in cocktail.components:
+        if cp._fp is None: continue
+
         w = 1.0
         if cp.molecular_weight is not None and (cp.unit == "w/v" or cp.unit == "% (w/v)" or cp.unit == "(w/v)" or cp.unit== "%w/v"):
             w = (((cp.conc * 0.01) * 200) * math.pow(10, -6)) * float(cp.molecular_weight)
@@ -55,5 +57,8 @@ def _centroid(cocktail):
         
     if cocktail.ph is not None:
         centroid['cockatoo_ph'] = cocktail.ph
+
+    if len(centroid) == 0:
+        return None
 
     return centroid
