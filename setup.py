a23b5e3773b5f4f3bbdaf6eac1fe7f2c7d3034ed
Andrew E. Bruno <aebruno2@buffalo.edu>
2013-12-13 12:01:37 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,25 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'cockatoo',
+    'name': 'cockatoo',
+    'description': 'cockatoo - A similarity metric for macromolecular crystallization conditions',
     'author': 'Andrew E. Bruno',
     'url': '',
     'download_url': '',
     'author_email': 'aebruno2@buffalo.edu',
     'version': '0.0.1',
-    'install_requires': ['nose'],
+    'install_requires': ['nose','rdkit'],
     'packages': ['cockatoo'],
     'scripts': [],
-    'name': 'cockatoo'
+    'classifiers': [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+     ],
 }
 
 setup(**config)
