506ff81fc1eeae19510e306bbf1f0d5f859974e1
Andrew E. Bruno <aebruno2@buffalo.edu>
2013-12-13 14:42:18 -0500

    Updated distuilts

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from distutils.core import setup
 
-config = {
-    'name': 'cockatoo',
-    'description': 'cockatoo - A similarity metric for macromolecular crystallization conditions',
-    'author': 'Andrew E. Bruno',
-    'url': '',
-    'download_url': '',
-    'author_email': 'aebruno2@buffalo.edu',
-    'version': '0.0.1',
-    'install_requires': ['nose','rdkit'],
-    'packages': ['cockatoo'],
-    'scripts': [],
-    'classifiers': [
+VERSION = '0.0.1'
+
+setup(
+    name='cockatoo',
+    description='cockatoo - A similarity metric for macromolecular crystallization conditions',
+    long_description='cockatoo - A similarity metric for macromolecular crystallization conditions',
+    author='Andrew E. Bruno',
+    url='https://github.com/ubccr/cockatoo',
+    license='GNU General Public License v3 (GPLv3)',
+    download_url='https://github.com/ubccr/cockatoo',
+    author_email='aebruno2@buffalo.edu',
+    version=VERSION,
+    requires=['nose'],
+    packages=['cockatoo'],
+    package_data={'cockatoo': ['data/*.csv']},
+    scripts=['bin/cockatoo-convert', 'bin/cockatoo-hclust'],
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
@@ -22,7 +24,5 @@ config = {
         'Programming Language :: Python',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
-     ],
-}
-
-setup(**config)
+     ]
+)
