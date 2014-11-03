5322073e7c89825e725f077c99143c1efe4f9a04
Andrew E. Bruno <aebruno2@buffalo.edu>
2014-11-03 10:35:49 -0500

    Fix marshmallow version <= 0.7.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.5.0'
+VERSION = '0.5.1'
 
 setup(
     name='cockatoo',
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         'click',
         'pinky',
-        'marshmallow',
+        'marshmallow<=0.7.0',
     ],
     entry_points='''
         [console_scripts]
